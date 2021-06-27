b64528a92d74781071ef4de5f8924c111dc9ab26
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-27 21:01:48 +0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={{fontSize:40,alignItems:'center'}}>Splash Screen</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
