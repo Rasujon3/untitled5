e0b0531a217b4122deaaa131907578ff9f8e48c0
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-27 22:47:47 +0600

    101 Device Info For Splash Screen And Other's "MyDeviceInfo: '',apiLevel:'', version:'',"
    Link: https://github.com/react-native-device-info/react-native-device-info
    
    cmnt : npm install --save react-native-device-info

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 class App extends Component {
 
     constructor() {
         super();
-        this.state={
-            MyDeviceInfo:"",
-        }
+        this.state = {
+            MyDeviceInfo: '',
+            apiLevel:'',
+            version:'',
+        };
     }
 
     componentDidMount() {
+        let DeviceID = DeviceInfo.getDeviceId();
+        this.setState({MyDeviceInfo: DeviceID});
+
+        let version = DeviceInfo.getVersion();
+        this.setState({version: version});
+
+        DeviceInfo.getApiLevel().then((apiLevel) => {
+            this.setState({apiLevel: apiLevel});
+        });
     }
 
     render() {
         return (
             <View>
-                <Text>{this.state.MyDeviceInfo}</Text>
+                <Text style={{fontSize: 40}}>{this.state.MyDeviceInfo}</Text>
+                <Text style={{fontSize: 40}}>{this.state.apiLevel}</Text>
+                <Text style={{fontSize: 40}}>{this.state.version}</Text>
             </View>
         );
     }
