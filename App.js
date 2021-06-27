0ef3fcbeea24c82aa7ddc1c8279975cc15810de3
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-27 21:04:28 +0600

    100 Device Info For Splash Sceen And Other's
    100 Device Info For Splash Sceen And Other's

--- App.js
+++ App.js
@@ -2,13 +2,25 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            MyDeviceInfo:"",
+        }
+    }
+
+    componentDidMount() {
+    }
+
     render() {
         return (
             <View>
-                <Text style={{fontSize:40,alignItems:'center'}}>Splash Screen</Text>
+                <Text>{this.state.MyDeviceInfo}</Text>
             </View>
         );
     }
 }
 
 export default App;
+
