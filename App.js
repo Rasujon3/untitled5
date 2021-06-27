9a798643e520a6eab20507c94e1d4e7cfbe99daa
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-28 00:56:38 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {PERMISSIONS, request} from 'react-native-permissions';
 
 class App extends Component {
+
+    componentDidMount() {
+        request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+            .then((result) => {
+
+            });
+    }
+
     render() {
         return (
             <View>
